3e8f3a3375d564dd4c701564b1a55f95943cd366
Chandaraprakash <chandaraprakash@gmail.com>
2014-08-20 23:36:23 -0700

    Tips
    Tips Calculator

--- Tips/ViewController.swift
+++ Tips/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
                             
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipPercentSelector: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tipLabel.text = "$0.00";
+        totalLabel.text = "$0.00";
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,46 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        
+        var billAmount = Double((billField.text as NSString).doubleValue)
+        var tipPercentages = [0.18, 0.2, 0.25]
+        var tipPercentage = tipPercentages[tipPercentSelector.selectedSegmentIndex]
+        var tip = billAmount * tipPercentage
+        var total = billAmount + tip
+        
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true);
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        var defaults = NSUserDefaults.standardUserDefaults()
+        var tipPercentage = Double((defaults.objectForKey("defaultTipPercentage") as NSString).doubleValue)
+        
+        if(billField.text != nil) {
+            var billAmount = Double((billField.text as NSString).doubleValue)
+
+            var tip = billAmount * (tipPercentage/100);
+            var total = billAmount + tip
+            
+            tipLabel.text = "$\(tip)"
+            totalLabel.text = "$\(total)"
+            
+            tipLabel.text = String(format: "$%.2f", tip)
+            totalLabel.text = String(format: "$%.2f", total)
+        }
+        
+    }
+    
+    
 }
 
